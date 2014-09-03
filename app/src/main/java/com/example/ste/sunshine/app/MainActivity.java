9952b93cd34a56ada4ccbcb9197c286c3c4d5988
Stephen Wootten <stewootten@gmail.com>
2014-09-03 19:49:39 +0100

    Added a comment

--- app/src/main/java/com/example/ste/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/ste/sunshine/app/MainActivity.java
@@ -14,7 +14,7 @@ import android.os.Build;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    // Comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
